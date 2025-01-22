8e0b8dc828b010d25afe7d56ac8b548ce309f7f3
github 200BingSu <github 200BingSu@gmail.com>
2025-01-22 17:44:38 +0900

    [feat] 일반 유저 회원가입 폼 기능 반영

--- src/pages/signup/SignUpIndex.jsx
+++ src/pages/signup/SignUpIndex.jsx
@@ -1,4 +1,11 @@
+import { Outlet } from "react-router-dom";
+
 const SignUpIndex = () => {
-  return <div>SignUpIndex</div>;
+  return (
+    <div>
+      <h1>SignUpIndex</h1>
+      {<Outlet />}
+    </div>
+  );
 };
 export default SignUpIndex;
