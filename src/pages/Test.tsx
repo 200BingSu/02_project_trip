b866df3b2a9bebc5e1635d26834efdd335d46af5
github 200BingSu <github 200BingSu@gmail.com>
2025-02-20 15:43:54 +0900

    [feat]소켓 시도

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,53 +1,12 @@
-import { Button, message, Steps, theme } from "antd";
-import JwtChat from "../components/chat/Jwtchat";
-import StompChat from "../components/chat/StompChat";
-import { useState } from "react";
 import Chat from "../components/chat/Chat";
 
 (window as any).global = window;
 
-const steps = [
-  {
-    title: "First",
-    content: "First-content",
-  },
-  {
-    title: "Second",
-    content: "Second-content",
-  },
-  {
-    title: "Last",
-    content: "Last-content",
-  },
-  {
-    title: "Last",
-    content: "Last-content",
-  },
-];
-
 const Test = () => {
-  const { token } = theme.useToken();
-  const [current, setCurrent] = useState<number>(0);
-
-  const onChange = (value: number) => {
-    console.log("onChange:", value);
-    setCurrent(value);
-  };
-  const next = () => {
-    setCurrent(current + 1);
-  };
-  const prev = () => {
-    setCurrent(current - 1);
-  };
-  const items = steps.map(item => ({
-    key: item.title,
-    title: item.title,
-  }));
-
   return (
     <div>
-      {/* <JwtChat />
-      <StompChat /> */}
+      {/* <JwtChat /> */}
+      {/* <StompChat /> */}
       <Chat />
       {/* <>
         <Steps
