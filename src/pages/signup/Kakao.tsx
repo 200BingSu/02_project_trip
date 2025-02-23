5041cb281a1030f3f0a8b47af49d809e22d3beec
200Bingsu <200bingsu@gmail.com>
2025-02-23 21:19:14 +0900

    [feat]웹소켓 준비 및 카카오 로그인 준비

--- src/pages/signup/Kakao.tsx
+++ src/pages/signup/Kakao.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { Spin } from "antd";
+import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 const Kakao = (): JSX.Element => {
-  return <div>kakao</div>;
+  // 쿼리스트링
+  const [searchParams] = useSearchParams();
+  const accessToken = searchParams.get("accessToken");
+  const userName = searchParams.get("userName");
+  const userEmail = searchParams.get("userEmail");
+  const userProfileImage = searchParams.get("userProfileImage");
+
+  //useState
+  const [kakaoLogin, setKakaoLogin] = useState<boolean>(false);
+  // 카카오 로그인 정보 받은 뒤 회원가입 api
+  return (
+    <div>
+      <Spin
+        spinning={kakaoLogin}
+        tip="카카오 로그인 진행 중..."
+        size="large"
+      ></Spin>
+    </div>
+  );
 };
 
 export default Kakao;
