1090a8859e88bf9be0d0ff6c26d9daa1fd152991
200BingSu <200bingsu@gmail.com>
2025-03-07 01:30:46 +0900

    [feat] 리뷰 퍼블리싱 완료

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,7 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { getCookie } from "../../utils/cookie";
+import { useEffect } from "react";
+import { ROLE } from "../../types/enum";
 
 const Layout = () => {
+  const userInfo = getCookie("user");
+  const { role } = userInfo;
+  const navigate = useNavigate();
   const path = window.location.pathname;
+  useEffect(() => {
+    if (role.includes(ROLE.BUSI)) {
+      navigate("/business");
+    }
+  }, [role]);
+
   return (
     <div className="max-w-[768px] min-w-xs mx-auto relative h-screen ">
       {/* {path === "/" ? <MainHeader /> : <Header />} */}
--- src/pages/business/review/ReviewIndex.tsx
+++ src/pages/business/review/ReviewIndex.tsx
@@ -2,18 +2,66 @@ import { useSearchParams } from "react-router-dom";
 import StrfInfo from "../../../components/business/StrfInfo";
 import TitleHeaderTs from "../../../components/layout/header/TitleHeaderTs";
 import ReviewItem from "../../../components/business/review/ReviewItem";
+import { useEffect, useState } from "react";
+import { IBusinessReview, IReview } from "../../../types/interface";
+import { Spin } from "antd";
+import { mockReview } from "../../../mock/Review";
+import axios from "axios";
+
+export interface IReviewItem extends IReview {
+  reviewReply?: string | null;
+}
+
+interface IGetReviewListRes {
+  code: string;
+  data: IReviewItem[];
+}
 
 const ReviewIndex = (): JSX.Element => {
   const [searchParams] = useSearchParams();
   const strfId = Number(searchParams.get("strfId"));
+
+  //useState
+  const [reviewList, setReviewList] = useState<IReviewItem[]>([]);
+  const [startIdx, setStartIdx] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  // API 리뷰 목록
+  const getReviewList = async (): Promise<IGetReviewListRes | null> => {
+    const url = "/api/business/review/all";
+    setIsLoading(true);
+    try {
+      const res = await axios.get<IGetReviewListRes>(
+        `${url}?start_idx=${startIdx}&page_size=10`,
+      );
+      const resultData = res.data;
+      console.log("리뷰 목록 조회", resultData);
+      if (resultData) {
+        setReviewList(resultData.data);
+        setStartIdx(startIdx + 10);
+      }
+      setIsLoading(false);
+      return resultData;
+    } catch (error) {
+      console.log("리뷰 목록 조회", error);
+      setIsLoading(false);
+      return null;
+    }
+  };
+  useEffect(() => {
+    getReviewList();
+  }, []);
   return (
     <div className="flex flex-col gap-5">
-      <TitleHeaderTs title="리뷰 관리" />
       <StrfInfo name="업체 이름" id={strfId} category="숙소" />
       {/* 리뷰 목록 */}
-      <section className="px-4 flex flex-col gap-5">
-        <ReviewItem />
-      </section>
+      <Spin spinning={isLoading}>
+        <section className="flex flex-col gap-10">
+          {mockReview.map((item, index) => (
+            <ReviewItem key={index} item={item} strfId={strfId} />
+          ))}
+        </section>
+      </Spin>
     </div>
   );
 };
