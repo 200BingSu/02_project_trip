d8a834c0c22bb9cb700d3077e9189f8cd153c116
ohdaeo <assa01389@gmail.com>
2025-03-12 19:06:18 +0900

    refactor 여전히 상품페이지 중

--- src/components/contents/Amenities.tsx
+++ src/components/contents/Amenities.tsx
@@ -1,20 +1,50 @@
-import { FaBed } from "react-icons/fa6";
+import { useEffect, useState } from "react";
 import { IoIosArrowRoundForward } from "react-icons/io";
+import jwtAxios from "../../apis/jwt";
+import { Iamenity } from "../../types/interface";
+import { matchAmenitiesIcon } from "../../utils/match";
 
-const Amenities = () => {
+const Amenities = ({ strfId }: { strfId: number }) => {
+  const [amens, setAmens] = useState<Iamenity[]>();
+
+  const getAmenities = async () => {
+    try {
+      const res = await jwtAxios.get(
+        `/api/detail/amenity?strf_id=${strfId}&category=숙소`,
+      );
+      setAmens(res.data.data);
+      console.log("편의시설 조회", res.data.data);
+    } catch (error) {
+      console.log("편의시설 조회", error);
+    }
+  };
+
+  useEffect(() => {
+    getAmenities();
+  }, []);
+
   return (
-    <div>
-      <div>
-        <h3>편의시설 및 서비스</h3>
-        <button>
-          더보기 <IoIosArrowRoundForward />
+    <div className="px-4 my-6">
+      <div className="flex items-center justify-between">
+        <h3 className="font-semibold text-slate-700 text-xl">
+          편의시설 및 서비스
+        </h3>
+        <button className="flex items-center gap-[6px] text-sm text-slate-400">
+          더보기 <IoIosArrowRoundForward className="text-xl" />
         </button>
       </div>
-      <ul>
-        <li>
-          <FaBed />
-          <p>침대</p>
-        </li>
+      <ul className="grid grid-flow-col gap-4 overflow-hidden w-full h-[90px] mt-3">
+        {amens?.map(amen => (
+          <li
+            key={amen.amenityId}
+            className="min-w-[57px] flex flex-col items-center justify-center gap-[6px] text-slate-700"
+          >
+            <i className="text-2xl ">
+              {matchAmenitiesIcon(amen.amenityId ?? 0)}
+            </i>
+            <p>{amen.amenityTitle}</p>
+          </li>
+        ))}
       </ul>
     </div>
   );
