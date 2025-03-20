43da719d187bc707c64deb79412955bdaa1f44ff
github 200BingSu <github 200BingSu@gmail.com>
2025-03-20 21:51:42 +0900

    [fixed]수정

--- src/components/search/SearchAll.jsx
+++ src/components/search/SearchAll.jsx
@@ -5,6 +5,7 @@ import SearchItem from "./SearchItem";
 import { Button } from "antd";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { moveTop } from "../../utils/moveTo";
+import NoData from "../common/NoData";
 
 const SearchAll = () => {
   // 쿼리
@@ -45,70 +46,85 @@ const SearchAll = () => {
   };
   return (
     <>
-      {/* 관광지 */}
-      <section className="flex flex-col gap-5 items-center mb-10">
-        <h2 className="w-full text-lg font-semibold text-slate-700">관광지</h2>
-        <ul className="w-full flex flex-col gap-5">
-          {tourData.map((item, index) => {
-            return <SearchItem key={index} item={item} />;
-          })}
-        </ul>
-        <Button
-          className="px-5 py-4 border border-slate-300 
+      {searchValue.searchData.length === 0 && (
+        <NoData content="검색 결과가 없습니다." />
+      )}
+      {searchValue.searchData.length > 0 && (
+        <>
+          {/* 관광지 */}
+          <section className="flex flex-col gap-5 items-center mb-10">
+            <h2 className="w-full text-lg font-semibold text-slate-700">
+              관광지
+            </h2>
+            <ul className="w-full flex flex-col gap-5">
+              {tourData.map((item, index) => {
+                return <SearchItem key={index} item={item} />;
+              })}
+            </ul>
+            <Button
+              className="px-5 py-4 border border-slate-300 
         rounded-3xl text-base text-slate-600"
-          onClick={() => handleClickMore(1)}
-        >
-          더보기
-        </Button>
-      </section>
-      {/* 숙소 */}
-      <section className="flex flex-col gap-5 items-center mb-10">
-        <h2 className="w-full text-lg font-semibold text-slate-700">숙소</h2>
-        <ul className="w-full flex flex-col gap-5">
-          {stayData.map((item, index) => {
-            return <SearchItem key={index} item={item} />;
-          })}
-        </ul>
-        <Button
-          className="px-5 py-4 border border-slate-300 
+              onClick={() => handleClickMore(1)}
+            >
+              더보기
+            </Button>
+          </section>
+          {/* 숙소 */}
+          <section className="flex flex-col gap-5 items-center mb-10">
+            <h2 className="w-full text-lg font-semibold text-slate-700">
+              숙소
+            </h2>
+            <ul className="w-full flex flex-col gap-5">
+              {stayData.map((item, index) => {
+                return <SearchItem key={index} item={item} />;
+              })}
+            </ul>
+            <Button
+              className="px-5 py-4 border border-slate-300 
         rounded-3xl text-base text-slate-600"
-          onClick={() => handleClickMore(2)}
-        >
-          더보기
-        </Button>
-      </section>
-      {/* 맛집 */}
-      <section className="flex flex-col gap-5 items-center mb-10">
-        <h2 className="w-full text-lg font-semibold text-slate-700">맛집</h2>
-        <ul className="w-full flex flex-col gap-5">
-          {restData.map((item, index) => {
-            return <SearchItem key={index} item={item} />;
-          })}
-        </ul>
-        <Button
-          className="px-5 py-4 border border-slate-300 
+              onClick={() => handleClickMore(2)}
+            >
+              더보기
+            </Button>
+          </section>
+          {/* 맛집 */}
+          <section className="flex flex-col gap-5 items-center mb-10">
+            <h2 className="w-full text-lg font-semibold text-slate-700">
+              맛집
+            </h2>
+            <ul className="w-full flex flex-col gap-5">
+              {restData.map((item, index) => {
+                return <SearchItem key={index} item={item} />;
+              })}
+            </ul>
+            <Button
+              className="px-5 py-4 border border-slate-300 
         rounded-3xl text-base text-slate-600"
-          onClick={() => handleClickMore(3)}
-        >
-          더보기
-        </Button>
-      </section>
-      {/* 축제 */}
-      <section className="flex flex-col gap-5 items-center mb-10">
-        <h2 className="w-full text-lg font-semibold text-slate-700">축제</h2>
-        <ul className="w-full flex flex-col gap-5">
-          {festData.map((item, index) => {
-            return <SearchItem key={index} item={item} />;
-          })}
-        </ul>
-        <Button
-          className="px-5 py-4 border border-slate-300 
+              onClick={() => handleClickMore(3)}
+            >
+              더보기
+            </Button>
+          </section>
+          {/* 축제 */}
+          <section className="flex flex-col gap-5 items-center mb-10">
+            <h2 className="w-full text-lg font-semibold text-slate-700">
+              축제
+            </h2>
+            <ul className="w-full flex flex-col gap-5">
+              {festData.map((item, index) => {
+                return <SearchItem key={index} item={item} />;
+              })}
+            </ul>
+            <Button
+              className="px-5 py-4 border border-slate-300 
         rounded-3xl text-base text-slate-600"
-          onClick={() => handleClickMore(4)}
-        >
-          더보기
-        </Button>
-      </section>
+              onClick={() => handleClickMore(4)}
+            >
+              더보기
+            </Button>
+          </section>
+        </>
+      )}
     </>
   );
 };
